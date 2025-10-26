5f603cab628a64a5b3d797f2ca4af69de7a348c3
Darlene250 <d.ayinkamiy@alustudent.com>
2025-10-26 15:48:19 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,1 +1,40 @@
+#!/usr/bin/python3
+"""
+Module to query Reddit API for hot posts.
 
+This module contains a function to retrieve and display the titles
+of the top 10 hot posts from a specified subreddit using the Reddit API.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    for a given subreddit.
+
+    Args:
+        subreddit: The name of the subreddit to query
+
+    Returns:
+        None
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        'User-Agent': 'linux:reddit.api.project:v1.0 (by /u/yourusername)'
+    }
+
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
+            for post in posts:
+                title = post.get('data', {}).get('title')
+                if title:
+                    print(title)
+        else:
+            print(None)
+    except Exception:
+        print(None)
